aad6b2b068581123c8ad998170523eb01a86e972
MaheedharReddy123 <32666080+MaheedharReddy123@users.noreply.github.com>
2017-10-11 14:14:59 +0530

    #firstcommit

---
+++ about.php
@@ -0,0 +1,6 @@
+<ul>
+    <li><a href="index.php"><h2><u>Home</u></h2></li>
+    <li><a href="about.php"><h2><u>About</u></h2></li>        
+</ul>
+
+<h1>About</h1>
---
+++ index.php
@@ -0,0 +1,14 @@
+<ul>
+    <li><a href="index.php"><h2><u>Home</u></h2></li>
+    <li><a href="about.php"><h2><u>About</u></h2></li>        
+</ul>
+
+
+<?php
+
+
+
+phpinfo();
+
+
+?>
